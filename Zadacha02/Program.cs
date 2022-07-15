3b697c95133bf5e3b77c450c2d49ec0bc2a7a94b
Aleksei <lexxbar@mail.ru>
2022-07-15 15:23:24 +0200

    Zd02

---
+++ Zadacha02/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter number 1:"); 
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number 2:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number 3:"); 
+int c = Convert.ToInt32(Console.ReadLine());
+int max;
+
+if( a > b )
+    max = a;
+else
+    max = b;
+if( c > max )
+    max = c;
+
+Console.WriteLine("Max num:" + max);        
