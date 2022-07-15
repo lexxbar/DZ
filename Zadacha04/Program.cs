fe019e79b8a470586fdd344410bdd3c8b30c1dac
Aleksei <lexxbar@mail.ru>
2022-07-15 15:59:18 +0200

    Zd04

---
+++ Zadacha04/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+
+while (result <= n)
+{
+   if (result % 2 == 0)
+       Console.Write ( result + " " );
+   result = result + 1;    
+}
